fde47a0cda3d92e1219e4123d83d1ffe85f4a201
bnc <1010744256@qq.com>
2020-10-12 17:42:40 +0800

    移除原来的web代码,移除thymeleaf依赖

--- src/main/resources/static/js/pagination.js
+++
@@ -1,4 +0,0 @@
-var pagination = Vue.component('pagination', {
-    props: ['to_page', 'content'],
-    template: `<a class="link item" v-html="content" v-on:click="$emit('change_page',to_page)"></a>`,
-});
